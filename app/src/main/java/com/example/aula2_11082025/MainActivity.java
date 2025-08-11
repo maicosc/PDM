66641fbb1254740afdc596f15e1ccd723191473c
Your Name <you@example.com>
2025-08-11 09:12:58 -0300

    Adicao de contador

--- app/src/main/java/com/example/aula2_11082025/MainActivity.java
+++ app/src/main/java/com/example/aula2_11082025/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula2_11082025;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    int contador;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        textView = findViewById(R.id.tv);
+        textView.setText(Integer.toString(contador));
+        textView.setOnClickListener(v -> {
+
+            contador++;
+            textView.setText(Integer.toString(contador));
+
         });
+
+
     }
 }
