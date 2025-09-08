8ab81872c240aaefa7d67f07cb792dbb373b24e2
Your Name <you@example.com>
2025-09-08 09:12:18 -0300

    Outra tela

--- app/src/main/java/com/example/aula2_11082025/ImcResultado.java
+++ app/src/main/java/com/example/aula2_11082025/ImcResultado.java
@@ -1,8 +1,6 @@
 package com.example.aula2_11082025;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ImcResultado extends AppCompatActivity {
 
-    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        button = findViewById(R.id.button5);
-        button.setOnClickListener( v -> {
-            Intent intent = new Intent(this, ImcResultado.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_imc_resultado);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
