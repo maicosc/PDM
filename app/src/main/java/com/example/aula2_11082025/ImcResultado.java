c297590442bb31068db7ea4970e80fa83beab9a7
Your Name <you@example.com>
2025-09-15 09:12:39 -0300

    Compartilhamento de dados

--- app/src/main/java/com/example/aula2_11082025/ImcResultado.java
+++ app/src/main/java/com/example/aula2_11082025/ImcResultado.java
@@ -1,6 +1,10 @@
 package com.example.aula2_11082025;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ImcResultado extends AppCompatActivity {
 
+    Button buon;
+    TextView tv;
+    ImageView Iv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_imc_resultado);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        tv = findViewById(R.id.tvResultado);
+        Iv=findViewById(R.id.imageViewPerfil);
+        Intent i = getIntent();
+        Bundle b = i.getExtras();
+        double peso = b.getDouble("peso");
+        double altura = b.getDouble("altura");
+        double imc = peso/(altura*altura);
+        tv.setText(Double.toString(imc));
     }
 }
--- app/src/main/java/com/example/aula2_11082025/MainActivity.java
+++ app/src/main/java/com/example/aula2_11082025/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.aula2_11082025;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,14 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText etPeso, etAltura;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button5);
+        etAltura=findViewById(R.id.etAltura);
+        etPeso=findViewById(R.id.etPeso);
         button.setOnClickListener( v -> {
             Intent intent = new Intent(this, ImcResultado.class);
+            Bundle b = new Bundle();
+            double peso = Double.parseDouble(etPeso.getText().toString());
+            double altura = Double.parseDouble(etAltura.getText().toString());
+            b.putDouble("peso",peso);
+            b.putDouble("altura",altura);
+            intent.putExtras(b);
             startActivity(intent);
         });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
