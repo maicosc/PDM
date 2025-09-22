12e1051de60df4df01b602a065471cdb2d1b6aec
Your Name <you@example.com>
2025-09-22 08:47:42 -0300

    salvo

--- app/src/main/java/com/example/aula2_11082025/TabuadaResultado.java
+++ app/src/main/java/com/example/aula2_11082025/TabuadaResultado.java
@@ -1,5 +1,6 @@
 package com.example.aula2_11082025;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +16,6 @@ public class TabuadaResultado extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tabuada_resultado);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        Intent i = i.getIntent();
     }
 }
