2b507f6683f090f6b8c75c7ca22c860ec4d145a3
Your Name <you@example.com>
2025-09-22 08:41:46 -0300

    Feito tela main

--- app/src/main/java/com/example/aula2_11082025/MainActivity.java
+++ app/src/main/java/com/example/aula2_11082025/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aula2_11082025;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int num;
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        num = Integer.parseInt(findViewById(R.id.etNum).toString());
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(this, TabuadaResultado.class);
+            Bundle b = new Bundle();
+            b.putInt("num", num);
+            intent.putExtras(b);
+            startActivity(intent);
         });
     }
 }
