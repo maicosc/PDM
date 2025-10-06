5e8aecd102015edd62688392698f49a7e2ad106c
Maico Corrêa <maico.sc@aluno.ifsc.edu.br>
2025-10-06 09:07:46 -0300

    Fim da atividade em aula

--- app/src/main/java/com/example/aula2_11082025/MainActivity.java
+++ app/src/main/java/com/example/aula2_11082025/MainActivity.java
@@ -1,7 +1,18 @@
 package com.example.aula2_11082025;
 
+
+
+
+
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -10,29 +21,80 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    String [] nomes = new String[] {"César", "José", "Romulo", "Clara", "Samanta", "Camila", "Gustavo", "Bilu", "Ambrósio"};
+    ArrayList <String> textos;
     ListView listView;
+    Button btSalvar;
+    EditText etUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         listView = findViewById(R.id.listView);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        etUser = findViewById(R.id.etTextoUser);
+        btSalvar =findViewById(R.id.btSalvar);
+        textos = new ArrayList<>();
+        etUser.setOnKeyListener((v, keyCode, event) -> {
+                if(event.getAction() == KeyEvent.KEYCODE_ENTER){
+                    if(etUser.getText().toString().trim().isEmpty()){
+                        Toast.makeText(getApplicationContext(),"Preencha o campo de texto! IDIOTA!!!", Toast.LENGTH_LONG).show();
+                    }else{
+                        textos.add(etUser.getText().toString());
+                        ((ArrayAdapter)listView.getAdapter()).notifyDataSetChanged();
+                        etUser.setText("");
+                        etUser.requestFocus();
+                    }
+                }
+                return false;
         });
+        btSalvar.setOnClickListener(v -> {
+
+            if(etUser.getText().toString().trim().isEmpty()){
+                Toast.makeText(getApplicationContext(),"Preencha o campo de texto! IDIOTA!!!", Toast.LENGTH_LONG).show();
+            }else{
+                textos.add(etUser.getText().toString());
+                ((ArrayAdapter)listView.getAdapter()).notifyDataSetChanged();
+                etUser.setText("");
+                etUser.requestFocus();
+            }
+
+
+        });
+
+
         ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(),
                 android.R.layout.simple_list_item_1,
                 android.R.id.text1,
-                nomes);
+                textos);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener((parent, view, position, id) -> {
-            Toast.makeText(getApplicationContext(),nomes[position], Toast.LENGTH_LONG).show();
+        listView.setOnItemLongClickListener((parent, view, position, id) -> {
+
+                AlertDialog.Builder caixa = new AlertDialog.Builder(this);
+                caixa.setTitle("Excluir item");
+                caixa.setMessage("Tem certeza que deseja excluir o item \""+ textos.get(position) +"\"?");
+                caixa.setPositiveButton("Sim", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        textos.remove(position);
+                        ((ArrayAdapter)listView.getAdapter()).notifyDataSetChanged();
+                    }
+                });
+                caixa.setNegativeButton("Não", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.dismiss();
+                    }
+                });
+            AlertDialog alerta = caixa.create();
+                alerta.show();
+
+
+            return false;
         });
     }
+
 }
